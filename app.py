138784d7cb3686476fbddd74f625c9012718dd7a
Ranjan Koirala <ranjan.koirala1998@gmail.com>
2021-04-30 10:46:55 +0545

    Initial commit

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from twilio.rest import Client
 from scrapper import get_body
 
-account_sid = "AC3baaf8b11c7ba6d6064fa41f2d2346f3"
-auth_token = "c51620a26d68b078623ddb45f9ff7895"
+account_sid = <Account SID>             # Use the actual account sid
+auth_token = <Authentication Token>     # Use the actual authentication token
 
-my_phone_num = "+9779862074364"
+phone_num = <recipient_number>          # Use the actual recipient number
 
 def send_sms():
     client = Client(account_sid, auth_token)
 
     sms = client.messages.create(
 
-        from_= "+17725772292",
+        from_= <sender_number>,         # Use the actual sender number
         body = get_body(),
-        to = my_phone_num
+        to = phone_num
 
     )
 
     print("Executed Successfully!!")
+
+send_sms()
 
 
 
