919e524bc9f9ecacd653465530c5f017d1c29c79
Ranjan Koirala <ranjan.koirala1998@gmail.com>
2021-04-30 13:49:06 +0545

    Initial commit

--- scheduler.py
+++ scheduler.py
@@ -5,9 +5,11 @@ from app import send_sms
 def job() :
     send_sms()
 
-schedule.every().day.at("10:15").do(job)
-schedule.every().day.at("12:15").do(job)
-schedule.every().day.at("15:15").do(job)
+schedule.every(10).minutes.do(job)
+
+# schedule.every().day.at("10:15").do(job)
+# schedule.every().day.at("12:15").do(job)
+# schedule.every().day.at("15:15").do(job)
 
 while True:
     schedule.run_pending()
